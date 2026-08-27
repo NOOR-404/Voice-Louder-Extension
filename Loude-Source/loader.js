4ef8b8c2c27126556c13ba03fd4f22c208551567
Dhaniyal Hossain Noor <daniyaln.hossai@gmail.com>
2026-08-27 16:28:29 +0600

    Delete Loude-Source directory

--- Loude-Source/loader.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-    const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('injector.js');
-    script.onload = function () { script.remove(); };
-    (document.head || document.documentElement).appendChild(script);
-})();
